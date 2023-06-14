2a431c397ccccb16e35560c892af3140bbb7432b
Longxi Yin <dragonxi@users.noreply.github.com>
2023-06-14 15:40:28 +0800

    added barebone api and nextjs app

---
+++ api/views.py
@@ -0,0 +1,22 @@
+import json
+import logging
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from lib import azure_ip
+
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the api index.")
+
+
+def lookup(request):
+    if 'ip' in request.GET:
+        r = azure_ip.lookup(request.GET["ip"])
+    else:
+        r = azure_ip.lookup('20.99.251.48')
+    r = json.dumps(r)
+    return HttpResponse(str(r))
